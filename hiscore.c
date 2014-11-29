a7615ddd603a6341baf0c23b4decf7a1e09a1a08
Zoë Blade <zoe@bytenoise.co.uk>
2014-11-29 14:56:28 +0000

    Make program work with arbitrary arguments
    The only accepted one of which is, at the moment, "centiped".
    
    This now only works if the hiscore binary is in the same directory
    as MAME, as the filename is automatically worked out and opened
    relative to that.

--- hiscore.c
+++ hiscore.c
@@ -1,59 +1,72 @@
 #include <stdio.h>
+#include <string.h>
+
+int centiped() {
+	int c[18] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
+	int count = 0;
+	FILE *fp;
+
+	if ((fp = fopen("nvram/centiped/earom", "r")) == NULL) {
+		printf("Sorry, I can't open nvram/centiped/earom\n");
+		return 1;
+	}
+
+	/* Read file into memory */
+	for (count = 0; count < 18; count++) {
+		c[count] = getc(fp);
+
+		if (count > 8) {
+			/* Initials */
+			if (c[count] == 0) {
+				/* Convert spaces */
+				c[count] = 0x20;
+			} else {
+				/* Convert A=1, B=2 etc into ASCII */
+				c[count] += 0x40;
+			}
+		}
+	}
+
+	fclose(fp);
+
+	/* Print memory to screen */
+	for (count = 0; count < 3; count++) {
+		/* Score */
+		/* Display hex values as if they're decimal, that old 6502 trick */
+		printf("%02x", c[count * 3 + 2]);
+		printf("%02x", c[count * 3 + 1]);
+		printf("%02x", c[count * 3]);
+
+		printf(" ");
+
+		/* Initials */
+		printf("%c", c[count * 3 + 9]);
+		printf("%c", c[count * 3 + 10]);
+		printf("%c", c[count * 3 + 11]);
+
+		printf("\n");
+	}
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	int c[18] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
-	int count = 0;
-	int row = 0;
+	int arg = 0;
+	int errors = 0;
 
 	if (argc == 1) {
 		printf("Please specify a machine name, e.g. \"centiped\"\n");
 		return 0;
 	}
 
-	while (--argc > 0) {
-		if ((fp = fopen(*++argv, "r")) == NULL) {
-			printf("hiscore: sorry, I can't open the file %s\n", *argv);
-			return 1;
-		}
-
-		/* Read file into memory */
-		for (count = 0; count < 18; count++) {
-			c[count] = getc(fp);
-
-			if (count > 8) {
-				/* Initials */
-				if (c[count] == 0) {
-					/* Convert spaces */
-					c[count] = 0x20;
-				} else {
-					/* Convert A=1, B=2 etc into ASCII */
-					c[count] += 0x40;
-				}
+	while (++arg < argc) {
+		if (strcmp(argv[arg], "centiped") == 0) {
+			if (centiped() == 1) {
+				errors = 1;
 			}
-		}
-
-		fclose(fp);
-
-		/* Print memory to screen */
-		for (count = 0; count < 3; count++) {
-			/* Score */
-			/* Display hex values as if they're decimal, that old 6502 trick */
-			printf("%02x", c[count * 3 + 2]);
-			printf("%02x", c[count * 3 + 1]);
-			printf("%02x", c[count * 3]);
-
-			printf(" ");
-
-			/* Initials */
-			printf("%c", c[count * 3 + 9]);
-			printf("%c", c[count * 3 + 10]);
-			printf("%c", c[count * 3 + 11]);
-
-			printf("\n");
 		}
 	}
 
-	return 0;
+	return errors;
 }
